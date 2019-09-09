66e1a15feb79fcf2773c1a0da945f7c789022409
yashwantraut41 <yashwantraut41@icloud.com>
2019-09-09 23:21:18 +0530

    Android Assignment
    android Window WithExit

---
+++ 05-WindowWithExit/app/src/main/java/com/RTR/win_Exit/MyView.java
@@ -0,0 +1,118 @@
+package com.RTR.win_Exit;
+
+//added by me
+import androidx.appcompat.widget.AppCompatTextView;
+import android.content.Context;
+import android.view.Gravity;
+import android.graphics.Color;
+
+//for MotionEvent
+import android.view.MotionEvent;
+
+//for GestureDetector
+import android.view.GestureDetector;
+
+//for OnGestureListener
+import android.view.GestureDetector.OnGestureListener;
+
+//for OnDoubleTapListener
+import android.view.GestureDetector.OnDoubleTapListener;
+
+//implement all the method from implemented class 
+public class MyView extends AppCompatTextView implements OnGestureListener,OnDoubleTapListener
+{	
+	private GestureDetector gestureDetector; //field class varaible
+	public MyView(Context context)
+	{
+		
+		super(context);
+		setTextColor(Color.rgb(255,128,0));
+		setTextSize(60);
+		setGravity(Gravity.CENTER);
+		setText("Hello World!!!");
+
+		gestureDetector=new GestureDetector(context,this,null,false);
+		gestureDetector.setOnDoubleTapListener(this);  //this means handler 
+		
+	}
+	
+	//handling ontouchevent is the most important beacause it triggers all gesture and tap events 
+	@Override
+	public boolean onTouchEvent(MotionEvent event)
+	{
+		//code
+		int eventaction=event.getAction();
+		if(!gestureDetector.onTouchEvent(event))
+			super.onTouchEvent(event);
+		return(true);
+	}
+	
+	//abstract method from OnDoubleTapListener 
+	
+	@Override
+	public boolean onDoubleTap(MotionEvent e)
+	{
+		setText("Double Tap");
+		return(true);
+	}
+	
+	@Override
+	public boolean onDoubleTapEvent(MotionEvent e)
+	{
+		return(true);
+	}
+	
+	//abstract method from OnDoubleTapListener
+	@Override
+	public boolean onSingleTapConfirmed(MotionEvent e)
+	{
+		setText("Single Tap");
+		return(true);
+	}
+	
+	//abstract method from OnGestureListener so must be implemented
+	@Override
+	public boolean onDown(MotionEvent e)
+	{
+		//we already wriiten onSingleTapConfirmed analogus behaviour 
+		return(true);
+	}
+
+	//abstract method from OnGestureListener so must be implemented
+	@Override
+	public boolean onFling(MotionEvent e1,MotionEvent e2,float velocityX,float velocityY)
+	{
+		return(true);
+	}
+
+	//abstract method from OnGestureListener so must be implemented
+	@Override
+	public void onLongPress(MotionEvent e)
+	{
+		setText("Long Press");
+	}
+	
+	//abstract method from OnGestureListener so must be implemented
+	@Override
+	public boolean onScroll(MotionEvent e1,MotionEvent e2,float distanceX,float distanceY)
+	{
+		System.exit(0);
+		return(true);
+	}
+	
+	//abstract method from OnGestureListener so must be implemented
+	@Override
+	public void onShowPress(MotionEvent e)
+	{
+		
+	}
+
+	//abstract method from OnGestureListener so must be implemented
+	@Override
+	public boolean onSingleTapUp(MotionEvent e)
+	{
+		return(true);
+	}
+
+
+}
