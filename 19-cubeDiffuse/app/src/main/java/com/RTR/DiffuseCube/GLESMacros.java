850e1397ca8f98cb26474be38a7e19a82fa11991
yashwantraut41 <yashwantraut41@icloud.com>
2020-01-29 17:52:23 +0530

    CubeDiffuse

---
+++ 19-cubeDiffuse/app/src/main/java/com/RTR/DiffuseCube/GLESMacros.java
@@ -0,0 +1,8 @@
+package com.RTR.DiffuseCube;
+public class GLESMacros
+{
+  public static final int AMC_ATTRIBUTE_POSITION=0;
+  public static final int AMC_ATTRIBUTE_COLOR=1;
+  public static final int AMC_ATTRIBUTE_NORMAL=3;
+  public static final int AMC_ATTRIBUTE_TEXCOORD0=3;
+}
---
+++ 19-cubeDiffuse/app/src/main/java/com/RTR/DiffuseCube/GLESView.java
@@ -0,0 +1,646 @@
+package com.RTR.DiffuseCube;
+
+//added by me
+
+import android.content.Context;
+import android.view.Gravity;
+import android.graphics.Color;
+
+//view which will support OpenGL
+import android.opengl.GLSurfaceView;
+
+//for OpenGL BUFFERS
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+//for matrix math
+import android.opengl.Matrix;
+
+//for OpenGL ES  version -3.2
+import android.opengl.GLES32;
+
+import javax.microedition.khronos.opengles.GL10;
+
+import javax.microedition.khronos.egl.EGLConfig;
+//for MotionEvent
+import android.view.MotionEvent;
+
+//for GestureDetector
+import android.view.GestureDetector;
+
+//for OnGestureListener
+import android.view.GestureDetector.OnGestureListener;
+
+//for OnDoubleTapListener
+import android.view.GestureDetector.OnDoubleTapListener;
+
+//implement all the method from implemented class
+public class GLESView extends GLSurfaceView implements GLSurfaceView.Renderer, OnGestureListener,OnDoubleTapListener
+{
+	private GestureDetector gestureDetector; //field class varaible
+	private final Context context;
+	private int vertexShaderObject;
+	private int fragmentShaderObject;
+	private int shaderProgramObject;
+	
+	private float angleCube=0.0f;
+	private float rotationSpeed=0.5f;
+
+	
+	private int[] vaoCube=new int[1];
+	private int[] vboCubePosition=new int[1];
+	private int[] vboCubeNormal=new int[1];
+	
+	private int mvUniform;
+	private int pUniform;
+	private int ldUniform;
+	private int kdUniform;
+	private int lightPositionUniform;
+	private int lKeyPressUniform;
+
+	private boolean bLight=true;
+	private float[]perspectiveProjectionMatrix=new float[16];
+	public GLESView(Context drawingContext)
+	{
+
+		super(drawingContext);
+		context=drawingContext;
+
+		setEGLContextClientVersion(3);
+		setRenderer(this);
+		setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+
+		gestureDetector=new GestureDetector(context,this,null,false);
+		gestureDetector.setOnDoubleTapListener(this);  //this means handler
+
+	}
+
+	//handling ontouchevent is the most important beacause it triggers all gesture and tap events
+	@Override
+	public boolean onTouchEvent(MotionEvent event)
+	{
+		//code
+		int eventaction=event.getAction();
+		if(!gestureDetector.onTouchEvent(event))
+			super.onTouchEvent(event);
+		return(true);
+	}
+
+	//abstract method from OnDoubleTapListener
+
+	@Override
+	public boolean onDoubleTap(MotionEvent e)
+	{
+		rotationSpeed-=0.5f;
+		return(true);
+	}
+
+	@Override
+	public boolean onDoubleTapEvent(MotionEvent e)
+	{
+		return(true);
+	}
+
+	//abstract method from OnDoubleTapListener
+	@Override
+	public boolean onSingleTapConfirmed(MotionEvent e)
+	{
+		rotationSpeed+=0.5f;	
+		return(true);
+	}
+
+	//abstract method from OnGestureListener so must be implemented
+
+
+	@Override
+	public boolean onDown(MotionEvent e)
+	{
+		//we already wriiten onSingleTapConfirmed analogus behaviour
+		return(true);
+	}
+
+	//abstract method from OnGestureListener so must be implemented
+	@Override
+	public boolean onFling(MotionEvent e1,MotionEvent e2,float velocityX,float velocityY)
+	{
+		return(true);
+	}
+
+	//abstract method from OnGestureListener so must be implemented
+	@Override
+	public void onLongPress(MotionEvent e)
+	{
+		if(bLight==true)
+		{
+			bLight=false;
+		}
+		else
+		{
+			bLight=true;
+		}
+	}
+
+	//abstract method from OnGestureListener so must be implemented
+	@Override
+	public boolean onScroll(MotionEvent e1,MotionEvent e2,float distanceX,float distanceY)
+	{
+		System.exit(0);
+		uninitialize();
+		return(true);
+	}
+
+	//abstract method from OnGestureListener so must be implemented
+	@Override
+	public void onShowPress(MotionEvent e)
+	{
+
+	}
+
+	//abstract method from OnGestureListener so must be implemented
+	@Override
+	public boolean onSingleTapUp(MotionEvent e)
+	{	
+		
+		return(true);
+	}
+
+	//IMPLEMENT GLSurfaceView.Renderer Method
+
+	@Override
+	public void onSurfaceCreated(GL10 gl, EGLConfig config)
+	{
+		String openglVersion=gl.glGetString(GL10.GL_VERSION);
+		System.out.println("AMC:"+openglVersion);
+
+		String shadingLanguageVersion=gl.glGetString(GLES32.GL_SHADING_LANGUAGE_VERSION);
+		System.out.println("AMC:"+shadingLanguageVersion);
+
+		String vendor=gl.glGetString(GL10.GL_VENDOR);
+		System.out.println("AMC:"+vendor);
+
+		String renderer=gl.glGetString(GL10.GL_RENDERER);
+		System.out.println("AMC:"+renderer);
+
+		initialize();
+	}
+
+	@Override
+	public void onSurfaceChanged(GL10 unused,int width, int height)
+	{
+		resize(width,height);
+	}
+
+
+	@Override
+	public void onDrawFrame(GL10 unused)
+	{
+		display();
+		update();
+	}
+
+	//our custom methods
+
+
+	private void initialize()
+	{
+		vertexShaderObject=GLES32.glCreateShader(GLES32.GL_VERTEX_SHADER);
+			//vertex shader code
+		final String vertexShaderSourceCode=
+		String.format
+		(
+		"#version 320 es" +
+		"\n" +
+		"in vec4 vPosition;" +
+		"in vec3 vNormal;" +
+		"uniform mat4 u_mv_matrix;"+
+		"uniform mat4 u_p_matrix;" +
+		"uniform mediump int u_lKeyPress;" +
+		"uniform vec3 u_ld ;" +
+		"uniform vec3 u_kd ;" +
+		"uniform vec3 u_light_position ;" +
+		"out vec3 diffuseColor;" +
+		"void main(void)" +
+		"{" +
+			"if(u_lKeyPress==1)"+
+			"{"+
+				"vec4 eye_coordinates=u_mv_matrix*vPosition;" +
+				"mat3 normalmatrix=mat3(transpose(inverse(u_mv_matrix)));" +
+				"vec3 tnorm=normalize(normalmatrix*vNormal);" +
+				"vec3 s=normalize(vec3(u_light_position-eye_coordinates.xyz));" +
+				"diffuseColor=u_ld*u_kd*dot(s,tnorm);" +
+			"}"+
+			"gl_Position=u_p_matrix*u_mv_matrix*vPosition;" +
+		"}"
+		);
+
+		//specify above shader source code to vertexShaderObject
+		//give shader source code
+		GLES32.glShaderSource(vertexShaderObject,vertexShaderSourceCode);
+
+		//compile the vertex shader code
+		GLES32.glCompileShader(vertexShaderObject);
+
+		//error checking code for vertex shader
+		int []iShaderCompileStatus=new int[1];
+		int []iInfoLogLength=new int[1];
+		String szInfoLog=null;
+
+		GLES32.glGetShaderiv(vertexShaderObject,GLES32.GL_COMPILE_STATUS,iShaderCompileStatus,0);
+
+		if(iShaderCompileStatus[0]==GLES32.GL_FALSE)
+		{
+			GLES32.glGetShaderiv(vertexShaderObject,GLES32.GL_INFO_LOG_LENGTH,iInfoLogLength,0);
+
+			if(iInfoLogLength[0]>0)
+			{
+				szInfoLog=GLES32.glGetShaderInfoLog(vertexShaderObject);
+				System.out.println("AMC vertex shader compilation Log:"+szInfoLog);
+				uninitialize();
+				System.exit(0);
+			}
+		}
+
+		//fragment shader
+		fragmentShaderObject=GLES32.glCreateShader(GLES32.GL_FRAGMENT_SHADER);
+
+		 final String fragmentShaderSourceCode=
+		 String.format
+		 (
+		 "#version 320 es" +
+		 "\n" +
+		 "precision highp float;" +
+		"in vec3 diffuseColor;" +
+		"out vec4 FragColor;" +
+		"uniform int u_lKeyPress ;" +
+		 "void main(void)" +
+		 "{" +
+			"if(u_lKeyPress==1)" +
+			"{" +
+				"FragColor=vec4(diffuseColor,1.0);" +
+			"}"+
+			"else" +
+			"{" +
+				"FragColor=vec4(1.0,1.0,1.0,1.0);" +
+			"}" +
+		 "}"
+		 );
+
+		 GLES32.glShaderSource(fragmentShaderObject,fragmentShaderSourceCode);
+
+		 GLES32.glCompileShader(fragmentShaderObject);
+		 iShaderCompileStatus[0]=0;
+		 iInfoLogLength[0]=0;
+		 szInfoLog=null;
+		 GLES32.glGetShaderiv(fragmentShaderObject,GLES32.GL_COMPILE_STATUS,iShaderCompileStatus,0);
+
+		 if(iShaderCompileStatus[0]==GLES32.GL_FALSE)
+		 {
+			 GLES32.glGetShaderiv(fragmentShaderObject,GLES32.GL_INFO_LOG_LENGTH,iInfoLogLength,0);
+
+			 if(iInfoLogLength[0]>0)
+			 {
+				 szInfoLog=GLES32.glGetShaderInfoLog(fragmentShaderObject);
+				 System.out.println("AMC fragment shader compilation log:"+szInfoLog);
+				 uninitialize();
+				 System.exit(0);
+			 }
+		 }
+		 	//create shader program object
+			shaderProgramObject=GLES32.glCreateProgram();
+
+			//attach vertex shader to shader program
+			GLES32.glAttachShader(shaderProgramObject,vertexShaderObject);
+
+			//attach fragment shader  to shaderprogram
+			GLES32.glAttachShader(shaderProgramObject,fragmentShaderObject);
+
+			//bind attributes to shader variable
+			GLES32.glBindAttribLocation(shaderProgramObject,GLESMacros.AMC_ATTRIBUTE_POSITION,"vPosition");
+			GLES32.glBindAttribLocation(shaderProgramObject,GLESMacros.AMC_ATTRIBUTE_NORMAL,"vNormal");
+
+			//NOW Link shader program
+			GLES32.glLinkProgram(shaderProgramObject);
+
+			int []iProgramLinkStatus=new int[1];
+			iInfoLogLength[0]=0;
+			szInfoLog=null;
+
+			GLES32.glGetProgramiv(shaderProgramObject,GLES32.GL_LINK_STATUS,iProgramLinkStatus,0);
+
+			if(iProgramLinkStatus[0]==GLES32.GL_FALSE)
+			{
+				GLES32.glGetProgramiv(shaderProgramObject,GLES32.GL_INFO_LOG_LENGTH,iInfoLogLength,0);
+
+				if(iInfoLogLength[0]>0)
+				{
+					szInfoLog=GLES32.glGetProgramInfoLog(shaderProgramObject);
+					System.out.println("AMC Shader Program Link Log:"+szInfoLog);
+					uninitialize();
+					System.exit(0);
+				}
+			}
+
+			mvUniform=GLES32.glGetUniformLocation(shaderProgramObject,"u_mv_matrix");
+			pUniform=GLES32.glGetUniformLocation(shaderProgramObject,"u_p_matrix");
+			ldUniform=GLES32.glGetUniformLocation(shaderProgramObject,"u_ld");
+			kdUniform=GLES32.glGetUniformLocation(shaderProgramObject,"u_kd");
+			lightPositionUniform=GLES32.glGetUniformLocation(shaderProgramObject,"u_light_position");
+			lKeyPressUniform=GLES32.glGetUniformLocation(shaderProgramObject,"u_lKeyPress");
+
+
+			
+
+			final float[] cubeVertices=new float[]
+			{
+				//top
+				 1.0f,1.0f,-1.0f,
+				-1.0f,1.0f,-1.0f,
+				-1.0f,1.0f,1.0f,
+				 1.0f,1.0f,1.0f,
+
+				//bottom
+				1.0f,-1.0f,-1.0f,
+				-1.0f,-1.0f,-1.0f,
+				-1.0f,-1.0f,1.0f,
+				1.0f,-1.0f,1.0f,
+
+				//front
+				1.0f,1.0f,1.0f,
+				-1.0f,1.0f,1.0f,
+				-1.0f,-1.0f,1.0f,
+				1.0f,-1.0f,1.0f,
+
+				//back
+				1.0f,1.0f,-1.0f,
+				-1.0f,1.0f,-1.0f,
+				-1.0f,-1.0f,-1.0f,
+				1.0f,-1.0f,-1.0f,
+
+				//right
+				1.0f,1.0f,-1.0f,
+				1.0f,1.0f,1.0f,
+				1.0f,-1.0f,1.0f,
+				1.0f,-1.0f,-1.0f,
+				
+				//left
+				- 1.0f,1.0f,1.0f,
+				-1.0f,1.0f,-1.0f,
+				-1.0f,-1.0f,-1.0f,
+				-1.0f,-1.0f,1.0f
+			};
+
+			final float[] cubeNormal=new float[]
+			{
+				//top
+				0.0f,1.0f,0.0f,
+				0.0f,1.0f,0.0f,
+				0.0f,1.0f,0.0f,
+				0.0f,1.0f,0.0f,
+				//bottom
+				0.0f,-1.0f,0.0f,
+				0.0f,-1.0f,0.0f,
+				0.0f,-1.0f,0.0f,
+				0.0f,-1.0f,0.0f,
+				//front
+				0.0f,0.0f,1.0f,
+				0.0f,0.0f,1.0f,
+				0.0f,0.0f,1.0f,
+				0.0f,0.0f,1.0f,
+		
+				//back
+				0.0f,0.0f,-1.0f,
+				0.0f,0.0f,-1.0f,
+				0.0f,0.0f,-1.0f,
+				0.0f,0.0f,-1.0f,
+
+				//right
+				1.0f,0.0f,0.0f,
+				1.0f,0.0f,0.0f,
+				1.0f,0.0f,0.0f,
+				1.0f,0.0f,0.0f,
+				//left
+				-1.0f,0.0f,0.0f,
+				-1.0f,0.0f,0.0f,
+				-1.0f,0.0f,0.0f,
+				-1.0f,0.0f,0.0f
+
+			};
+
+		
+			//--------------------------CUBE ---------------------------------------------->>>>>
+
+			
+			//<<<<<------------------START VAO FOR CUBE ----------------------------->>>>
+			
+			//create vertex array object for CUBE (vao)
+			GLES32.glGenVertexArrays(1,vaoCube,0);
+
+			//bind vao for CUBE
+			GLES32.glBindVertexArray(vaoCube[0]);
+
+
+			//<<<<<---------START VBO FOR CUBE POSITION ---------------------------->>>>>
+			//create buffer for cube position 
+			GLES32.glGenBuffers(1,vboCubePosition,0);
+
+			//bind buffer
+			GLES32.glBindBuffer(GLES32.GL_ARRAY_BUFFER,vboCubePosition[0]);
+
+
+			//create buffer from our cubeVertices array so we can pass it to glBufferData()
+
+			//allocate buffer directly  from native memeory (not VM memory) thats why allocateDirect
+
+			ByteBuffer byteBuffer=ByteBuffer.allocateDirect(cubeVertices.length*4);
+
+
+			//arrange the byteorder of buffer in native byte order little indian/big indian
+
+			byteBuffer.order(ByteOrder.nativeOrder());
+
+			//create the float type buffer and convert our byte type buffer into float type BUFFER
+
+			FloatBuffer positionBuffer=byteBuffer.asFloatBuffer();
+
+			//now put our array of triangle into these cooked buffer
+			positionBuffer.put(cubeVertices);
+
+			//set array at zeroth position of BUFFER
+			positionBuffer.position(0);
+
+			GLES32.glBufferData(GLES32.GL_ARRAY_BUFFER,cubeVertices.length*4,positionBuffer,GLES32.GL_STATIC_DRAW);
+
+			GLES32.glVertexAttribPointer(GLESMacros.AMC_ATTRIBUTE_POSITION,3,GLES32.GL_FLOAT,false,0,0);
+			GLES32.glEnableVertexAttribArray(GLESMacros.AMC_ATTRIBUTE_POSITION);
+
+			//unbind vbo for cube position  
+			GLES32.glBindBuffer(GLES32.GL_ARRAY_BUFFER,0);
+
+			//<<<<<<------------END VBO FOR CUBE POSITION------------------------------------------------>>>>
+
+			
+
+
+
+
+
+
+			//<<<<<-------------START VBO FOR CUBE NORMAL-------------------------->>>>>>>
+			GLES32.glGenBuffers(1,vboCubeNormal,0);
+			GLES32.glBindBuffer(GLES32.GL_ARRAY_BUFFER,vboCubeNormal[0]);
+
+			//create buffer from our cubeNormal array so we can pass it to glBufferData()
+			//allocate buffer directly  from native memeory (not VM memory) thats why allocateDirect
+			 byteBuffer=ByteBuffer.allocateDirect(cubeNormal.length*4);
+			//arrange the byteorder of buffer in native byte order little indian/big indian
+			byteBuffer.order(ByteOrder.nativeOrder());
+			//create the float type buffer and convert our byte type buffer into float type BUFFER
+			 positionBuffer=byteBuffer.asFloatBuffer();
+			//now put our array of cube color into these cooked buffer
+			positionBuffer.put(cubeNormal);
+			//set array at zeroth position of BUFFER
+			positionBuffer.position(0);
+			GLES32.glBufferData(GLES32.GL_ARRAY_BUFFER,cubeNormal.length*4,positionBuffer,GLES32.GL_STATIC_DRAW);
+			GLES32.glVertexAttribPointer(GLESMacros.AMC_ATTRIBUTE_NORMAL,3,GLES32.GL_FLOAT,false,0,0);
+			GLES32.glEnableVertexAttribArray(GLESMacros.AMC_ATTRIBUTE_NORMAL);
+
+			//unbind vbo for cube position  
+			GLES32.glBindBuffer(GLES32.GL_ARRAY_BUFFER,0);
+
+			//<<<<<<--------------- END VBO FOR  CUBE COLOR ------------->>>>>>>>>>>>>>
+
+			//unbind vao fo cube 
+			GLES32.glBindVertexArray(0);
+
+			//<<<<<<<<-----------------------END VAO FOR CUBE--------------------------------->>>>>>
+
+			GLES32.glEnable(GLES32.GL_DEPTH_TEST);
+			//disable cull face so we can see back side of geometry while rotation
+			GLES32.glDisable(GLES32.GL_CULL_FACE);
+			GLES32.glDepthFunc(GLES32.GL_LEQUAL);
+			GLES32.glClearColor(0.0f,0.0f,0.0f,1.0f);
+
+			Matrix.setIdentityM(perspectiveProjectionMatrix,0);
+	}
+
+	private void resize(int width,int height)
+	{
+		if(height==0)
+		{
+			height=1;
+		}
+		GLES32.glViewport(0,0,width,height);
+		Matrix.perspectiveM(perspectiveProjectionMatrix,0,45.0f,(float)width/(float)height,0.1f,100.0f);
+
+	}
+
+	private void display()
+	{
+		GLES32.glClear(GLES32.GL_COLOR_BUFFER_BIT|GLES32.GL_DEPTH_BUFFER_BIT);
+		GLES32.glUseProgram(shaderProgramObject);
+
+		//declaration of matrices
+		float[] modelViewMatrix=new float[16];
+		float[] rotationMatrix=new float[16];
+		float[] translationaMatrix=new float[16];
+		float[] scaleMatrix=new float[16];
+
+		//set iDDENTITY OF MATRICES
+		Matrix.setIdentityM(modelViewMatrix,0);
+		Matrix.setIdentityM(scaleMatrix,0);
+		Matrix.setIdentityM(rotationMatrix,0);
+		Matrix.setIdentityM(translationaMatrix,0);		
+	
+		if (bLight == true)
+		{
+			GLES32.glUniform1i(lKeyPressUniform, 1);
+			GLES32.glUniform3f(ldUniform, 1.0f, 1.0f, 1.0f);
+			GLES32.glUniform3f(kdUniform, 0.5f, 0.5f, 0.5f);
+			GLES32.glUniform4f(lightPositionUniform, 0.0f, 0.0f, 2.0f, 1.0f);
+		}
+		else
+		{
+			GLES32.glUniform1i(lKeyPressUniform, 0);
+		}
+	
+		Matrix.translateM(translationaMatrix,0,0.0f,0.0f,-5.0f);
+		Matrix.scaleM(scaleMatrix,0,0.75f,0.75f,0.75f);
+		Matrix.rotateM(rotationMatrix,0,angleCube,1.0f,1.0f,1.0f);
+		
+		Matrix.multiplyMM(modelViewMatrix,0,translationaMatrix,0,scaleMatrix,0);
+		Matrix.multiplyMM(modelViewMatrix,0,modelViewMatrix,0,rotationMatrix,0);
+
+		//send this matrices to shader
+		GLES32.glUniformMatrix4fv(mvUniform,1,false,modelViewMatrix,0);
+		GLES32.glUniformMatrix4fv(pUniform,1,false,perspectiveProjectionMatrix,0);
+
+
+		//BIND WITH vao of cube
+		GLES32.glBindVertexArray(vaoCube[0]);
+		GLES32.glDrawArrays(GLES32.GL_TRIANGLE_FAN,0,4);
+		GLES32.glDrawArrays(GLES32.GL_TRIANGLE_FAN,4,4);
+		GLES32.glDrawArrays(GLES32.GL_TRIANGLE_FAN,8,4);
+		GLES32.glDrawArrays(GLES32.GL_TRIANGLE_FAN,12,4);
+		GLES32.glDrawArrays(GLES32.GL_TRIANGLE_FAN,16,4);
+		GLES32.glDrawArrays(GLES32.GL_TRIANGLE_FAN,20,4);
+		GLES32.glBindVertexArray(0);
+
+		GLES32.glUseProgram(0);
+		requestRender();
+	}
+	private void update()
+	{	
+		angleCube+=rotationSpeed;
+		if(angleCube >= 360.0f)
+		{
+			angleCube=0.0f;
+		}
+	
+	}
+	private void uninitialize()
+	{
+		if(vboCubeNormal[0]!=0)
+		{
+			GLES32.glDeleteBuffers(1,vboCubeNormal,0);
+			vboCubeNormal[0]=0;
+		}
+
+		if(vboCubePosition[0]!=0)
+		{
+			GLES32.glDeleteBuffers(1,vboCubePosition,0);
+			vboCubePosition[0]=0;
+		}
+
+		if(vaoCube[0]!=0)
+		{
+			GLES32.glDeleteVertexArrays(1,vaoCube,0);
+			vaoCube[0]=0;
+		}
+
+
+		if(shaderProgramObject!=0)
+		{
+			int[] shaderCount=new int[1];
+			int shaderNumber;
+			GLES32.glUseProgram(shaderProgramObject);
+
+			GLES32.glGetProgramiv(shaderProgramObject,GLES32.GL_ATTACHED_SHADERS,shaderCount,0);
+			int[] shader=new int[shaderCount[0]];
+			if(shader[0]!=0)
+			{
+				GLES32.glGetAttachedShaders(shaderProgramObject,shaderCount[0],shaderCount,0,shader,0);
+
+				for(shaderNumber=0;shaderNumber<shaderCount[0];shaderNumber++)
+				{
+					GLES32.glDetachShader(shaderProgramObject,shader[shaderNumber]);
+					GLES32.glDeleteShader(shader[shaderNumber]);
+					shader[shaderNumber]=0;
+				}
+			}
+			GLES32.glUseProgram(0);
+		GLES32.glDeleteShader(shaderProgramObject);
+		shaderProgramObject=0;
+		}
+	}
+}
